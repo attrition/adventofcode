9d9a465ebdb44ff48a3c5f6de85efea5e534144d
Chris Walsh <cwalsh62@bloomberg.net>
2019-12-10 05:16:30 -0500

    day5

--- 2019/day5/computer.ts
+++ 2019/day5/computer.ts
@@ -4,8 +4,28 @@ export class MemoryBank {
     constructor(memory: number[]) {
         this.memory = [...memory];
     }
+    
+    read(address: number, mode: Mode): number {
+        switch (mode) {
+            case Mode.POSITION:
+                return this.memory[this.memory[address]];
+            case Mode.IMMEDIATE:
+                return this.memory[address];
+        }
+        return 0;
+    }
+    
+    write(address: number, value: number, mode: Mode): void {
+        switch (mode) {
+            case Mode.POSITION:
+                this.memory[this.memory[address]] = value;
+                break;
+            case Mode.IMMEDIATE:
+                this.memory[address] = value;
+                break;
+        }
+    }
 }
-
 
 const enum Mode {
     POSITION = 0,
@@ -17,53 +37,76 @@ export class Computer {
     initialMemory: MemoryBank;
     runningMemory: MemoryBank;
     running: Boolean;
-    
-    constructor(memoryBank: MemoryBank) {
+    debug: Boolean;
+
+    constructor(memoryBank: MemoryBank, debug?: Boolean) {
         this.initialMemory = new MemoryBank(memoryBank.memory);
         this.running = true;
+        this.debug = debug || false;
     }
 
     read(address: number, mode: Mode): number {
-        switch (mode) {
-            case Mode.POSITION:
-                return this.runningMemory.memory[this.runningMemory.memory[address]];
-            case Mode.IMMEDIATE:
-                return this.runningMemory.memory[address];
-        }
-        return 0;
+        return this.runningMemory.read(address, mode);
     }
 
     write(address: number, value: number, mode: Mode): void {
-        switch (mode) {
-            case Mode.POSITION:
-                this.runningMemory[this.runningMemory[address]] = value;
-                break;
-            case Mode.IMMEDIATE:
-                this.runningMemory[address] = value;
-                break;
-        }
+        return this.runningMemory.write(address, value, mode);
     }
 
     add(address: number, modes: Mode[]): number {
-        let a = this.read(address + 1, modes[0]);
-        let b = this.read(address + 2, modes[1]);
-        this.write(address + 3, a + b, modes[2]);
-        return 3;
+        let a = this.read(address++, modes[0]);
+        let b = this.read(address++, modes[1]);
+        this.write(address++, a + b, modes[2]);
+        return address;
     }
 
     multiply(address: number, modes: Mode[]): number {
-        let a = this.read(address + 1, modes[0]);
-        let b = this.read(address + 2, modes[1]);
-        this.write(address + 3, a * b, modes[2]);
-        return 3;
+        let a = this.read(address++, modes[0]);
+        let b = this.read(address++, modes[1]);
+        this.write(address++, a * b, modes[2]);
+        return address;
     }
 
     store(address: number, modes: Mode[]): number {
-        return 0;
+        this.write(address++, this.input, modes[0]);
+        return address;
     }
 
     output(address: number, modes: Mode[]): number {
-        return 0;
+        console.log(this.read(address++, modes[0]));
+        return address;
+    }
+
+    jumpIfTrue(address: number, modes: Mode[]): number {
+        let test = this.read(address++, modes[0]);
+        let jumpAddress = this.read(address++, modes[1]);
+        return (test != 0) ? jumpAddress : address;
+    }
+
+    jumpIfFalse(address: number, modes: Mode[]): number {
+        let test = this.read(address++, modes[0]);
+        let jumpAddress = this.read(address++, modes[1]);
+        return (test == 0) ? jumpAddress : address;
+    }
+
+    lessThan(address: number, modes: Mode[]): number {
+        let a = this.read(address++, modes[0]);
+        let b = this.read(address++, modes[1]);
+        let where = this.read(address++, Mode.IMMEDIATE);
+        let result = (a < b) ? 1 : 0;
+
+        this.write(where, result, Mode.IMMEDIATE);
+        return address;
+    }
+
+    equals(address: number, modes: Mode[]): number {
+        let a = this.read(address++, modes[0]);
+        let b = this.read(address++, modes[1]);
+        let where = this.read(address++, Mode.IMMEDIATE);
+        let result = (a == b) ? 1 : 0;
+
+        this.write(where, result, Mode.IMMEDIATE);
+        return address;
     }
 
     fetchInstruction(instrPtr: number): [ number, Mode[] ]{
@@ -85,50 +128,81 @@ export class Computer {
         return [ opCode, modes ];
     }
 
-    executeInstruction(instrPtr: number) {
+    debugRunningMemory(instrPtr: number): void {
+        let instrMap = [ "noop", "add", "mul", "wri", "out", "jt", "jf", "lt", "eq" ]
+        let debug = "";
+        for (let i = 0; i < this.runningMemory.memory.length; ++i) {
+            let mem = this.runningMemory.memory[i] % 100;
+            if (instrPtr == i) {
+                debug += "*" + (instrMap[mem] || mem) + "*";
+            }  else {
+                debug += this.runningMemory.memory[i];
+            }
+            if (i != this.runningMemory.memory.length - 1) { debug += "\t"; }
+        }
+        console.log(debug);
+    }
+
+    executeInstruction(instrPtr: number): number {
+
+        if (this.debug) {
+            this.debugRunningMemory(instrPtr);
+        }
 
         let instruction = this.fetchInstruction(instrPtr);
+        instrPtr++;
+
         let opCode = instruction[0];
         let modes = instruction[1];
 
-        let instrSize = 0;
-
         switch (opCode) {
-            case 1: {
-                instrSize = this.add(instrPtr, modes);
+            case 1:
+                instrPtr = this.add(instrPtr, modes);
                 break;
-            }
 
-            case 2: {
-                instrSize = this.multiply(instrPtr, modes);
+            case 2:
+                instrPtr = this.multiply(instrPtr, modes);
                 break;
-            }
 
-            case 3: {
+            case 3:
+                instrPtr = this.store(instrPtr, modes);
                 break;
-            }
 
-            case 4: {
+            case 4:
+                instrPtr = this.output(instrPtr, modes);
                 break;
-            }
+            
+            case 5:
+                instrPtr = this.jumpIfTrue(instrPtr, modes);
+                break;
 
-            case 99: {
+            case 6:
+                instrPtr = this.jumpIfFalse(instrPtr, modes);
+                break;
+
+            case 7:
+                instrPtr = this.lessThan(instrPtr, modes);
+                break;
+
+            case 8:
+                instrPtr = this.equals(instrPtr, modes);
+                break;
+
+            case 99:
                 this.running = false;
                 break;
-            }
         }
 
-        // increment instrPtr by opcode + instruction size
-        return instrPtr + 1 + instrSize;
+        return instrPtr;
     }
 
     patch(patchTuples: [number, number][]) {
         patchTuples.forEach(t => {
-            this.runningMemory.memory[t[0]] = t[1];
+            this.write(t[0], t[1], Mode.IMMEDIATE);
         });
     }
 
-    run(input: number, patches: [number, number][]): number {
+    run(input: number, patches?: [number, number][]): void {
 
         this.input = input;
         this.runningMemory = new MemoryBank(this.initialMemory.memory);
@@ -138,7 +212,5 @@ export class Computer {
         while (this.running) {
             instrPtr = this.executeInstruction(instrPtr);
         }
-
-        return this.runningMemory.memory[0];
     }
 }
